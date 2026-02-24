b9ef8ee32155aa69958f88a0542b1fd895b7b0ac
Moses Odhiambo <106585122+Moses361@users.noreply.github.com>
2026-02-24 23:45:32 +0300

    CustomerReview.jsx & Subscribe.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Nav } from "./components";
 import {
   Hero,
   PopularProducts,
@@ -8,34 +9,33 @@ import {
 
 const App = () => {
   return (
-    <main className='relative w-full overflow-x-hidden'>
+    <main className="relative w-full overflow-x-hidden">
       <Nav />
-     <section className='xl:padding-l wide:padding-r padding-b overflow-x-hidden'>
+
+      {/* Hero */}
+      <section className="w-full overflow-x-hidden xl:padding-l wide:padding-r padding-b">
         <Hero />
       </section>
-      <section className='w-full overflow-x-hidden padding'>
+
+      {/* Popular Products */}
+      <section className="w-full overflow-x-hidden padding">
         <PopularProducts />
       </section>
-      <section className='w-full overflow-x-hidden padding'>
+
+      {/* Super Quality */}
+      <section className="w-full overflow-x-hidden padding">
         <SuperQuality />
       </section>
-      <section className='padding-x py-10'>
+
+      {/* Services */}
+      <section className="w-full overflow-x-hidden padding-x py-10">
         <Services />
       </section>
-      {<section className='bg-pale-blue padding'>
+
+      {/* Customer Reviews */}
+      <section className="w-full overflow-x-hidden bg-pale-blue padding">
         <CustomerReviews />
-      </section>}
-
-
-      {/* // <section className='padding-x sm:py-32 py-16 w-full'>
-      //   <Subscribe />
-      // </section>
-      // <section className=' bg-black padding-x padding-t pb-8'>
-      //   <Footer />
-      // </section> */}
-      {/* {<section className='padding'>
-        <SpecialOffer />
-      </section> */}
+      </section>
     </main>
   );
 };
--- src/sections/index.js
+++ src/sections/index.js
@@ -2,7 +2,7 @@ import Hero from "./Hero";
 import PopularProducts from "./PopularProducts";
 import SuperQuality from "./SuperQuality";
 import Services from "./Services";
-import CustomerReviews from "./Review";
+import CustomerReviews from "./CustomerReviews";
 // import SpecialOffer from "./SpecialOffer";
 // import Subscribe from "./Subscribe";
 // import Footer from "./Footer";
